85d4e58d68c5381f4eda33e91a8a70c3dc55a0c4
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2020-08-14 20:09:42 -0300

    Start project

---
+++ src/test/java/com/deveficiente/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
